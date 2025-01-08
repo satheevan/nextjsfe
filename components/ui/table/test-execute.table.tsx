6e1640460565cadd2665ca1738b0c974625c7dca
saravanan <velus515@gmail.com>
2025-01-08 22:16:01 +0530

    params changed

--- components/ui/table/test-execute.table.tsx
+++ components/ui/table/test-execute.table.tsx
@@ -11,6 +11,7 @@ import {
 } from "@nextui-org/react";
 import Link from "next/link";
 import { PaginationContainer } from "../pagination-container";
+import { useProjectStore } from "@/store/project.store";
 
 const columns = [
   {
@@ -42,32 +43,45 @@ type Project = {
   id: number;
   projectName: string;
   totalTest: number;
-  totalSuits:number;
+  totalSuits: number;
 };
 
 interface ProjectTableProps {
   data: Project[]; // Array of user objects
 }
 
-const TestExecuteTable: React.FC<ProjectTableProps> = ({data}) => {
- 
+const TestExecuteTable: React.FC<ProjectTableProps> = ({ data }) => {
+  const { currentProjectId } = useProjectStore();
 
-  function MultiColorProgressbar(value1:number,value2:number,value3:number) {
+  function MultiColorProgressbar(
+    value1: number,
+    value2: number,
+    value3: number
+  ) {
     return (
       <div className="w-full flex bg-gray-200 rounded-full h-6 dark:bg-gray-700">
-        <div className="bg-greenClr h-6 rounded-full rounded-e-none flex justify-center items-center" style={{width: `${value1}%`}}>
+        <div
+          className="bg-greenClr h-6 rounded-full rounded-e-none flex justify-center items-center"
+          style={{ width: `${value1}%` }}
+        >
           <span className="text-white text-xs font-thin">{`${value1}%`}</span>
         </div>
-        <div className="bg-redClr h-6 flex justify-center items-center" style={{width: `${value2}%`}}>
+        <div
+          className="bg-redClr h-6 flex justify-center items-center"
+          style={{ width: `${value2}%` }}
+        >
           <span className="text-white text-xs font-thin">{`${value2}%`}</span>
         </div>
-        <div className="bg-yellowClr h-6 rounded-e-full flex justify-center items-center" style={{width: `${value3}%`}}>
+        <div
+          className="bg-yellowClr h-6 rounded-e-full flex justify-center items-center"
+          style={{ width: `${value3}%` }}
+        >
           <span className="text-white text-xs font-thin">{`${value3}%`}</span>
         </div>
       </div>
     );
   }
-  
+
   // const rowData = data.map((project)=>(
   //   {
   //     key:project?.id,
@@ -89,7 +103,7 @@ const TestExecuteTable: React.FC<ProjectTableProps> = ({data}) => {
       key: "1",
       testsuitename: (
         <Link
-          href={`/projects/inventory/testexe/1`}
+          href={`/projects/${currentProjectId}/testexe/1`}
           className="text-xs font-medium text-btnColorProject"
         >
           Smoke Test
@@ -97,13 +111,13 @@ const TestExecuteTable: React.FC<ProjectTableProps> = ({data}) => {
       ),
       when: "Mon,Sep at: 14:20:04",
       totaltests: "25",
-      result:MultiColorProgressbar(55,20,25),
+      result: MultiColorProgressbar(55, 20, 25),
     },
     {
       key: "2",
       testsuitename: (
         <Link
-          href={`/projects/inventory/testexe/1`}
+          href={`/projects/${currentProjectId}/testexe/1`}
           className="text-xs font-medium text-btnColorProject"
         >
           Smoke Test
@@ -111,13 +125,13 @@ const TestExecuteTable: React.FC<ProjectTableProps> = ({data}) => {
       ),
       when: "Mon,Sep at: 14:20:04",
       totaltests: "25",
-      result:MultiColorProgressbar(55,20,25),
+      result: MultiColorProgressbar(55, 20, 25),
     },
     {
       key: "3",
       testsuitename: (
         <Link
-          href={`/projects/inventory/testexe/1`}
+          href={`/projects/${currentProjectId}/testexe/1`}
           className="text-xs font-medium text-btnColorProject"
         >
           Smoke Test
@@ -125,13 +139,13 @@ const TestExecuteTable: React.FC<ProjectTableProps> = ({data}) => {
       ),
       when: "Mon,Sep at: 14:20:04",
       totaltests: "25",
-      result:MultiColorProgressbar(55,30,15),
+      result: MultiColorProgressbar(55, 30, 15),
     },
     {
       key: "4",
       testsuitename: (
         <Link
-          href={`/projects/inventory/testexe/1`}
+          href={`/projects/${currentProjectId}/testexe/1`}
           className="text-xs font-medium text-btnColorProject"
         >
           Smoke Test
@@ -139,28 +153,34 @@ const TestExecuteTable: React.FC<ProjectTableProps> = ({data}) => {
       ),
       when: "Mon,Sep at: 14:20:04",
       totaltests: "25",
-      result:MultiColorProgressbar(15,25,60),
+      result: MultiColorProgressbar(15, 25, 60),
     },
   ];
 
   return (
     <div>
-    <Table aria-label="Example table with dynamic content" className="mt-4">
-      <TableHeader columns={columns} className="">
-        {(column) => <TableColumn key={column.key} className="">{column.label}</TableColumn>}
-      </TableHeader>
-      <TableBody items={rows} className="">
-        {(item) => (
-          <TableRow key={item.key} className="">
-          {/* <TableRow key={item.key} className=""> */}
-            {(columnKey) => (
-              <TableCell className={`${columnKey==="result"?"":""}`}>{getKeyValue(item, columnKey)}</TableCell>
-            )}
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-    <PaginationContainer/>
+      <Table aria-label="Example table with dynamic content" className="mt-4">
+        <TableHeader columns={columns} className="">
+          {(column) => (
+            <TableColumn key={column.key} className="">
+              {column.label}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={rows} className="">
+          {(item) => (
+            <TableRow key={item.key} className="">
+              {/* <TableRow key={item.key} className=""> */}
+              {(columnKey) => (
+                <TableCell className={`${columnKey === "result" ? "" : ""}`}>
+                  {getKeyValue(item, columnKey)}
+                </TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      <PaginationContainer />
     </div>
   );
 };
